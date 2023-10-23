ca55a9e0a43bdb0e468a5560aaf2b8a553f393c9
RyanMKrol <ryankrol.m@gmail.com>
2023-10-23 23:41:41 +0100

    Add cookie modal handling, and loading more events

--- src/scripts/exploreEvents.js
+++ src/scripts/exploreEvents.js
@@ -1,38 +1,57 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-await-in-loop */
+
 import puppeteer from 'puppeteer';
 
 (async function main() {
   // Launch the browser and open a new blank page
-  console.log(1);
   const browser = await puppeteer.launch({ headless: false });
-  console.log(2);
   const page = await browser.newPage();
-  console.log(3);
 
   // Navigate the page to a URL
   await page.goto('https://www.roundhouse.org.uk/whats-on/?type=event');
 
-  console.log(4);
   // Set screen size
   await page.setViewport({ width: 1080, height: 1024 });
-  console.log(5);
 
-  // Wait and click on first result
+  await maybeAcknowledgeCookieModal(page);
+
+  // Wait for something that signifies the page is loaded
   const searchResultSelector = '.card-view';
-  console.log(7);
   await page.waitForSelector(searchResultSelector);
 
-  const events = await page.$$('.event-card');
+  while (true) {
+    const events = await page.$$('.event-card');
 
-  const eventItemDetails = await events.reduce((acc, event) => acc.then(async (newAcc) => {
-    const artist = await event.$eval('.event-card__title', (result) => result.textContent.trim());
-    const date = await event.$eval('.event-card__details', (result) => result.textContent.trim());
-    newAcc.push({ artist, date });
-    return newAcc;
-  }), Promise.resolve([]));
+    const eventItemDetails = await events.reduce((acc, event) => acc.then(async (newAcc) => {
+      const artist = await event.$eval('.event-card__title', (result) => result.textContent.trim());
+      const date = await event.$eval('.event-card__details', (result) => result.textContent.trim());
+      newAcc.push({ artist, date });
+      return newAcc;
+    }), Promise.resolve([]));
 
-  console.log(eventItemDetails);
+    console.log(eventItemDetails, eventItemDetails.length);
 
+    const doesTheButtonExist = await page.$('.button--loadmore');
+    if (!doesTheButtonExist) {
+      console.log('the button does not exist apparently');
+      break;
+    }
+
+    await page.click('.button--loadmore');
+  }
   await browser.close();
 }());
+
+/**
+ * Acknowledges a cookie modal if one is present
+ * @param {object} page The browser page object
+ */
+async function maybeAcknowledgeCookieModal(page) {
+  const cookieModal = await page.$('.cookie-policy__popup');
+
+  if (cookieModal) {
+    cookieModal.$eval('.button--primary', (el) => el.click());
+  }
+}
